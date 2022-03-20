c72bfdccecd25776ecce05ef1ccbd6babe23b50c
Maksym Zinchenko <siqsuruq@gmail.com>
2022-03-20 14:48:11 -0100

    Init

---
+++ general/sql/INIT_DATA.sql
@@ -0,0 +1,23 @@
+INSERT INTO "platform" ("uuid_platform","name") VALUES 
+( 'd1a5ad90-2534-471f-9a7f-b9eae64f0e7e','Tcl' ),
+( '9bb9da21-a628-4cca-8e94-5ab9317b6747','Tcl/Tk' ),
+( '99810a9b-2759-4bb5-a6d0-7b4f04b2668c','Linux32' ),
+( '3c1e96e2-f130-4d5c-b206-63623007ca58','Linux64' ),
+( '13df1bad-20e6-4891-ac94-aad937c80bd1','Win32' ),
+( '9c8237da-68ea-46c2-971f-2c59ce888329','Win64' ),
+( '255506df-6f5f-416b-be9f-9f2315db4eae','Mac' );
+
+INSERT INTO "category" ("uuid_category","name") VALUES 
+( '1611ab61-626c-4070-8f1a-f63e9af1a0e1','Essentials' ),
+( '7bc6bb01-f6e7-4d1e-91b1-b98ef52dd110','Database' ),
+( '57f2b402-e605-4e0e-a8d2-11c9f000356b','Networking' ),
+( '7696807e-c339-4c08-ba73-ec7f06f25dc7','Web' ),
+( 'b7c11cbf-6a16-4ebb-8e12-b403ac17dff6','XML' ),
+( '65657bfb-663b-4784-a437-f1cfc92f09a0','GUI' ),
+( 'af5bd1fb-1001-4c43-a221-b43100529eab','Widgets' ),
+( '0f434733-b639-4864-a48f-0032adbf7313','Cryptography' ),
+( '6925c028-8fa0-470d-a08d-955ede949441','OO' ),
+( 'f5fd64c2-cff0-4c8c-81d5-154eb04a6369','Images' ),
+( 'd433f81c-298e-4514-bc35-efd55e4dda0f','Audio' ),
+( '17ef93bd-ddee-40fe-a148-923135238348','Video' ),
+( '8563bb67-63cc-460b-9787-0fb29cb675e3','Miscellaneous' );
---
+++ general/sql/TABLES.sql
@@ -0,0 +1,81 @@
+-- CREATE  TABLE "platform" -------------------------------------
+CREATE  TABLE "platform" ( 
+	"id" BIGSERIAL,
+	"uuid_platform" UUid DEFAULT gen_random_uuid() NOT NULL,
+	"name" Text UNIQUE NOT NULL,
+	"extra" "hstore" DEFAULT ''::hstore NOT NULL,
+	PRIMARY KEY ( "id", "uuid_platform" ) );
+ ;
+-- -------------------------------------------------------------
+
+-- CREATE INDEX "idx_platform" ---------------------------------
+CREATE INDEX "idx_platform" ON "platform" USING btree( "id" Asc NULLS Last );
+-- -------------------------------------------------------------
+
+-- CREATE  TABLE "category" -------------------------------------
+CREATE  TABLE "category" ( 
+	"id" BIGSERIAL,
+	"uuid_category" UUid DEFAULT gen_random_uuid() NOT NULL,
+	"name" Text UNIQUE NOT NULL,
+	"parent_category" UUid,
+	"extra" "hstore" DEFAULT ''::hstore NOT NULL,
+	PRIMARY KEY ( "id","uuid_category" ) );
+ ;
+-- -------------------------------------------------------------
+
+-- CREATE INDEX "idx_category" ---------------------------------
+CREATE INDEX "idx_category" ON "category" USING btree( "uuid_category" Asc NULLS Last );
+-- -------------------------------------------------------------
+
+-- CREATE  TABLE "license" --------------------------------------
+CREATE  TABLE "license" ( 
+	"id" BIGSERIAL,
+	"uuid_license" UUid DEFAULT gen_random_uuid() NOT NULL,
+	"name" Text UNIQUE NOT NULL,
+	"extra" "hstore" DEFAULT ''::hstore NOT NULL,
+	"link" Text,
+	PRIMARY KEY ( "id", "uuid_license" ) );
+ ;
+-- -------------------------------------------------------------
+
+-- CREATE INDEX "idx_license" ----------------------------------
+CREATE INDEX "idx_license" ON "license" USING btree( "id" Asc NULLS Last );
+-- -------------------------------------------------------------
+
+-- CREATE  TABLE "package" --------------------------------------
+CREATE  TABLE "package" ( 
+	"id" BIGSERIAL,
+	"uuid_package" UUid DEFAULT gen_random_uuid() NOT NULL,
+	"name" Text NOT NULL,
+	"uuid_platform" UUid NOT NULL,
+	"description" Text NOT NULL,
+	"uuid_category" UUid NOT NULL,
+	"uuid_license" UUid NOT NULL,
+	"path" Text NOT NULL,
+	"extra" "hstore" DEFAULT ''::hstore NOT NULL,
+	PRIMARY KEY ( "id", "uuid_package" ) );
+ ;
+-- -------------------------------------------------------------
+
+-- CREATE INDEX "idx_package" ----------------------------------
+CREATE INDEX "idx_package" ON "package" USING btree( "id" Asc NULLS Last );
+-- -------------------------------------------------------------
+
+-- CREATE  TABLE "package_metadata" -----------------------------
+CREATE  TABLE "package_metadata" ( 
+	"id" BIGSERIAL,
+	"uuid_package_metadata" UUid DEFAULT gen_random_uuid() NOT NULL,
+	"uuid_package" UUid NOT NULL,
+	"identifier" Text NOT NULL,
+	"version" Text NOT NULL,
+	"title" Text NOT NULL,
+	"creator" Text NOT NULL,
+	"contributor" Text NOT NULL,
+	"rights" Text NOT NULL,
+	"URL" Text NOT NULL,
+	PRIMARY KEY ( "id", "uuid_package_metadata" ) );
+ ;
+-- -------------------------------------------------------------
+-- CREATE INDEX "idx_package_metadata" ----------------------------------
+CREATE INDEX "idx_package_metadata" ON "package_metadata" USING btree( "id" Asc NULLS Last );
+-- -------------------------------------------------------------
