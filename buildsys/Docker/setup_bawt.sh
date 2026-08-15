a9cef4219748c0a4c644bdab489ee7d087fbc20c
Maksym Zinchenko <siqsuruq@gmail.com>
2026-08-15 17:56:20 -0100

    Remove bawt download

--- buildsys/Docker/setup_bawt.sh
+++ buildsys/Docker/setup_bawt.sh
@@ -7,9 +7,3 @@ echo "--> Creating persistent build directories in $BAWT_DIR..."
 sudo mkdir -p "$BAWT_DIR/Setup"
 sudo mkdir -p "$BAWT_DIR/InputLibs"
 sudo chown -R "$USER":"$USER" "$BAWT_DIR"
-
-echo "--> Fetching BAWT framework toolchain scripts..."
-cd "$BAWT_DIR"
-curl -L -s -O https://tcl3d.org
-unzip -q -j Bawt-3.3.0.zip -d "$BAWT_DIR"
-rm Bawt-3.3.0.zip
